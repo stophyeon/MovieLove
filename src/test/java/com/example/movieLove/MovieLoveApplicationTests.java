1e44019fb1a051c371acdedc8ad0b0838e67063a
stophyeon <jeongjihyeon401@gmail.com>
2023-08-09 15:50:03 +0900

    프로젝트 구조

---
+++ src/test/java/com/example/movieLove/MovieLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieLove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
